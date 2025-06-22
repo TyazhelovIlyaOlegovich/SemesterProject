5be2d5675f935a0c8f4bc16fff0184a40a2bc452
TyazhelovIlyaOlegovich <ilatazelov076@gmail.com>
2025-06-23 02:16:53 +0300

    Сейвы!
    Забыл в инвентарь сейв сделать, теперь он есть и вроде работает

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,10 +1,13 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+#include <nlohmann/json.hpp>
 #include "Inventory.hpp"
 
+using json = nlohmann::json;
+
 Inventory::Inventory(int max_health, int health, int max_mana, int mana, int level, int experience, int money, std::vector<std::pair<std::string, int>>& item) {
-		for (auto temp : item) { 
+		for (const auto& temp : item) {
 			items[temp.first] = temp.second; 
 		}
 		stats["max_hp"] = max_health;
@@ -16,7 +19,7 @@ Inventory::Inventory(int max_health, int health, int max_mana, int mana, int lev
 		stats["gold"] = money;
 	}
 
-int Inventory::get_stat(std::string& stat) {
+int Inventory::get_stat(std::string stat) {
 	return stats[stat];
 }
 
@@ -41,4 +44,17 @@ int Inventory::get_item(std::string item) {
 
 void Inventory::set_item(std::string item, int amount) {
 	items[item] += amount;
+}
+
+void Inventory::save(json& save_data) {
+	save_data["max_hp"] = get_stat("max_hp");
+	save_data["hp"] = get_stat("hp");
+	save_data["max_mana"] = get_stat("max_mana");
+	save_data["mana"] = get_stat("mana");
+	save_data["lvl"] = get_stat("lvl");
+	save_data["exp"] = get_stat("exp");
+	save_data["gold"] = get_stat("gold");
+	for (const auto& pair : items) {
+		save_data[pair.first] = pair.second;
+	}
 }
--- Inventory.hpp
+++ Inventory.hpp
@@ -1,28 +1,25 @@
 #ifndef INVENTORY
 #define INVENTORY
 
+using json = nlohmann::json;
+
 class Inventory {
 private:
-	/**int max_hp{};
-	int hp{};
-	int max_mana{};
-	int mana{};
-	int lvl{};
-	int xp{};
-	int gold{};*/
 	std::unordered_map<std::string, int> items{};
 	std::unordered_map<std::string, int> stats{};
 public:
 	const int max_hp{};
 	Inventory(int experience, int health, int max_health, int mana, int max_mana, int level, int money, std::vector<std::pair<std::string, int>>& item);
 
-	int get_stat(std::string& stat);
+	int get_stat(std::string stat);
 
 	void set_stat(std::string& stat, int amount);
 
 	int get_item(std::string item);
 
 	void set_item(std::string item, int amount);
+
+	void save(json& save_data);
 
 };
 
